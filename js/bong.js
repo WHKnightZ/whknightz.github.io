70835d052cefe7224b7dd2eb928bccc006ee4e33
Dealer <nguyenkhanhsl1997@gmail.com>
2019-10-16 16:31:30 +0700

    update

--- js/bong.js
+++ js/bong.js
@@ -16,85 +16,85 @@ var canvas,
 	PI,
 	TWOPI;
 
-function rand( min, max ) {
-	return Math.random() * ( max - min ) + min;
+function rand(min, max) {
+	return Math.random() * (max - min) + min;
 }
 
-function randInt( min, max ) {
-	return Math.floor( min + Math.random() * ( max - min + 1 ) );
+function randInt(min, max) {
+	return Math.floor(min + Math.random() * (max - min + 1));
 };
 
 function Dot() {
 	this.x = width / 2;
 	this.y = height / 2;
-	this.vx = rand( -2, 2 );
-	this.vy = rand( -2, 2 );
-	this.radius = rand( 5, 15 );
-	this.color = randInt( 1, colorCount - 1 );
+	this.vx = rand(-2, 2);
+	this.vy = rand(-2, 2);
+	this.radius = rand(5, 15);
+	this.color = randInt(1, colorCount - 1);
 }
 
-Dot.prototype.step = function( i ) {
+Dot.prototype.step = function (i) {
 	// apply forces	
 	this.x += this.vx;
 	this.y += this.vy;
-		
+
 	// handle bounce	
-	if( this.vx > 0 && this.x + this.radius >= width ) {
+	if (this.vx > 0 && this.x + this.radius >= width) {
 		this.vx *= -0.6;
 	}
-	
-	if( this.vx < 0 && this.x - this.radius <= 0 ) {
+
+	if (this.vx < 0 && this.x - this.radius <= 0) {
 		this.vx *= -0.6;
 	}
-	
-	if( this.vy > 0 && this.y + this.radius >= height ) {
+
+	if (this.vy > 0 && this.y + this.radius >= height) {
 		this.vy *= -0.6;
 	}
-	
-	if( this.vy < 0 && this.y - this.radius <= 0 ) {
+
+	if (this.vy < 0 && this.y - this.radius <= 0) {
 		this.vy *= -0.6;
 	}
-	
+
 	// handle bounds and friction	
-	if( this.x + this.radius > width ) {
+	if (this.x + this.radius > width) {
 		this.x = width - this.radius;
 		this.vy *= friction;
 	}
-	
-	if( this.x - this.radius < 0 ) {
+
+	if (this.x - this.radius < 0) {
 		this.x = this.radius;
 		this.vy *= friction;
 	}
-	
-	if( this.y + this.radius > height ) {
+
+	if (this.y + this.radius > height) {
 		this.y = height - this.radius;
 		this.vx *= friction;
 	}
-	
-	if( this.y - this.radius < 0 ) {
+
+	if (this.y - this.radius < 0) {
 		this.y = this.radius;
 		this.vx *= friction;
 	}
-	
+
 	// handle gravity	
 	this.vx += xGravity;
 	this.vy += yGravity;
 };
 
-Dot.prototype.collide = function( otherDot ) {
+Dot.prototype.collide = function (otherDot) {
 	// still working on understanding this
 	// lots of help from http://lamberta.github.io/html5-animation/
 	var dx = otherDot.x - this.x,
 		dy = otherDot.y - this.y,
-		dist = Math.sqrt( dx * dx + dy * dy ),
+		dist = Math.sqrt(dx * dx + dy * dy),
 		minDist = this.radius + otherDot.radius;
-	if( dist < minDist ) {
+	if (dist < minDist) {
 		var tx = this.x + dx / dist * minDist,
 			ty = this.y + dy / dist * minDist,
-			ax = ( tx - otherDot.x ) * 0.6,
-			ay = ( ty - otherDot.y ) * 0.6;
+			ax = (tx - otherDot.x) * 0.6,
+			ay = (ty - otherDot.y) * 0.6;
 		this.vx -= ax;
-		this.vy -= ay;      
+		this.vy -= ay;
 		otherDot.vx += ax;
 		otherDot.vy += ay;
 		this.vx *= friction * 0.9;
@@ -104,16 +104,16 @@ Dot.prototype.collide = function( otherDot ) {
 	}
 };
 
-Dot.prototype.draw = function() {
+Dot.prototype.draw = function () {
 	ctx.beginPath();
-	ctx.arc( this.x, this.y, this.radius, 0, TWOPI );
-	ctx.fillStyle = palettes[ paletteCurrent ][ this.color ];
+	ctx.arc(this.x, this.y, this.radius, 0, TWOPI);
+	ctx.fillStyle = palettes[paletteCurrent][this.color];
 	ctx.fill();
 };
 
 function init() {
-	canvas = document.getElementById( 'canvas' );
-	ctx = canvas.getContext( '2d' );
+	canvas = document.getElementById('canvas');
+	ctx = canvas.getContext('2d');
 	xGravity = 0;
 	yGravity = 1;
 	friction = 0.99;
@@ -154,11 +154,11 @@ function init() {
 		]
 	];
 	paletteCount = palettes.length;
-	paletteCurrent = 3;	
-	colorCount = palettes[ 0 ].length;
+	paletteCurrent = 3;
+	colorCount = palettes[0].length;
 	PI = Math.PI;
 	TWOPI = PI * 2;
-	
+
 	reset();
 	loop();
 }
@@ -170,77 +170,77 @@ function reset() {
 	tick = 0;
 	mx = width / 2;
 	my = height / 2;
-	
+
 	canvas.width = width;
 	canvas.height = height;
 }
 
 function create() {
-	if( tick && dots.length < 500 ) {
-		dots.push( new Dot() );	
+	if (tick && dots.length < 500) {
+		dots.push(new Dot());
 	}
 }
 
 function step() {
 	var i = dots.length;
-	while( i-- ) {
-		dots[ i ].step( i );	
+	while (i--) {
+		dots[i].step(i);
 	}
-	
+
 	i = dots.length;
-	while( i-- ) {
-		dot = dots[ i ];
+	while (i--) {
+		dot = dots[i];
 		var j = i;
-		if( j > 0 ) {
-			while( j-- ) {
-				dot.collide( dots[ j ] );
+		if (j > 0) {
+			while (j--) {
+				dot.collide(dots[j]);
 			}
 		}
 	}
 }
 
 function draw() {
-	ctx.fillStyle = palettes[ paletteCurrent ][ 0 ];
-	ctx.fillRect( 0, 0, width, height );
-	
+	ctx.fillStyle = palettes[paletteCurrent][0];
+	ctx.fillRect(0, 0, width, height);
+
 	var i = dots.length;
-	while( i-- ) {
-		dots[ i ].draw();	
+	while (i--) {
+		dots[i].draw();
 	}
 }
 
 function loop() {
-	requestAnimationFrame( loop );
+	requestAnimationFrame(loop);
 	create();
 	step();
 	draw();
 	tick++;
 }
 
-function onmousemove( e ) {
+function onmousemove(e) {
 	mx = e.pageX;
 	my = e.pageY;
-	
-	xGravity = ( mx - width / 2 ) / ( width / 2 );
-	yGravity = ( my - height / 2 ) / ( height / 2 );
+
+	xGravity = (mx - width / 2) / (width / 2);
+	yGravity = (my - height / 2) / (height / 2);
 }
 
 function onmousedown() {
 	var i = dots.length;
-	while( i-- ) {
-		dots[ i ].vx += rand( -10, 10 );
-		dots[ i ].vy += rand( -10, 10 );
+	while (i--) {
+		dots[i].vx += rand(-10, 10);
+		dots[i].vy += rand(-10, 10);
 	}
 
-	if( paletteCurrent < paletteCount - 1 ) {
-		paletteCurrent++;	
+	if (paletteCurrent < paletteCount - 1) {
+		paletteCurrent++;
 	} else {
-		paletteCurrent = 0;	
+		paletteCurrent = 0;
 	}
 }
 
-window.addEventListener( 'resize', reset );
-window.addEventListener( 'mousemove', onmousemove );
-window.addEventListener( 'mousedown', onmousedown );
+window.addEventListener('resize', reset);
+window.addEventListener('mousemove', onmousemove);
+window.addEventListener('mousedown', onmousedown);
 
 init();
